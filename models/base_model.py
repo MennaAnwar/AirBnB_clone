c8542822a389c0d3bcd2aff5d5403be9312cfe7d
MennaAnwar <menna_m20@yahoo.com>
2023-07-13 08:04:30 +0000

    5. Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import models
 
 """ BaseModel class """
 class BaseModel:
@@ -21,6 +22,7 @@ class BaseModel:
       self.id = str(uuid.uuid4())
       self.created_at = datetime.now()
       self.updated_at = datetime.now()
+      models.storage.new(self)
     
   def __str__(self):
     return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
@@ -28,7 +30,8 @@ class BaseModel:
   
   def save(self):
     self.updated_at = datetime.now()
-    
+    models.storage.save()
+
   def to_dict(self):
     my_dict = self.__dict__.copy()
     my_dict["created_at"] = self.created_at.isoformat()
