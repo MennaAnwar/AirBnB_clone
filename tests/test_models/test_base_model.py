e83b0125d7932d5ab843cfd4f35de09b3364d1a2
Ahmed Hany <dev.ahmedhany@gmail.com>
2023-07-17 02:04:54 +0300

    remove __init__

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,7 +2,7 @@
 """ """
 from models.base_model import BaseModel
 import unittest
-import datetime
+from datetime import datetime
 from uuid import UUID
 import json
 import os
@@ -11,37 +11,39 @@ import os
 class test_basemodel(unittest.TestCase):
     """ """
 
-    def __init__(self, *args, **kwargs):
-        """ """
-        super().__init__(*args, **kwargs)
-        self.name = 'BaseModel'
-        self.value = BaseModel
-
+    @classmethod
     def setUp(self):
-        """ """
-        pass
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
 
+    @classmethod
     def tearDown(self):
         try:
-            os.remove('file.json')
-        except:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
             pass
 
     def test_default(self):
         """ """
-        i = self.value()
-        self.assertEqual(type(i), self.value)
+        i = BaseModel()
+        self.assertEqual(type(i), BaseModel)
 
     def test_kwargs(self):
         """ """
-        i = self.value()
+        i = BaseModel()
         copy = i.to_dict()
         new = BaseModel(**copy)
         self.assertFalse(new is i)
 
     def test_kwargs_int(self):
         """ """
-        i = self.value()
+        i = BaseModel()
         copy = i.to_dict()
         copy.update({1: 2})
         with self.assertRaises(TypeError):
@@ -49,22 +51,22 @@ class test_basemodel(unittest.TestCase):
 
     def test_save(self):
         """ Testing save """
-        i = self.value()
+        i = BaseModel()
         i.save()
-        key = self.name + "." + i.id
+        key = 'BaseModel' + "." + i.id
         with open('file.json', 'r') as f:
             j = json.load(f)
             self.assertEqual(j[key], i.to_dict())
 
     def test_str(self):
         """ """
-        i = self.value()
-        self.assertEqual(str(i), '[{}] ({}) {}'.format(self.name, i.id,
+        i = BaseModel()
+        self.assertEqual(str(i), '[{}] ({}) {}'.format('BaseModel', i.id,
                          i.__dict__))
 
     def test_todict(self):
         """ """
-        i = self.value()
+        i = BaseModel()
         n = i.to_dict()
         self.assertEqual(i.to_dict(), n)
 
@@ -72,22 +74,22 @@ class test_basemodel(unittest.TestCase):
         """ """
         n = {None: None}
         with self.assertRaises(TypeError):
-            new = self.value(**n)
+            new = BaseModel(**n)
 
     def test_id(self):
         """ """
-        new = self.value()
+        new = BaseModel()
         self.assertEqual(type(new.id), str)
 
     def test_created_at(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.created_at), datetime.datetime)
+        new = BaseModel()
+        self.assertEqual(type(new.created_at), datetime)
 
     def test_updated_at(self):
         """ """
-        new = self.value()
-        self.assertEqual(type(new.updated_at), datetime.datetime)
+        new = BaseModel()
+        self.assertEqual(type(new.updated_at), datetime)
         n = new.to_dict()
         new = BaseModel(**n)
         self.assertTrue(new.created_at == new.updated_at)
